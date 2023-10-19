4101a48230092ee02835633dcb1e0dc8517d13c6
Granth Gaurav <69439823+granthgg@users.noreply.github.com>
2023-10-19 19:48:46 +0530

    Create Footer.jsx

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-black text-white py-4">
+      <div className="container mx-auto text-center">
+        <p>© 2023 AI-powered Legal Assistant LEGAL DASTAVEZ. All rights reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
