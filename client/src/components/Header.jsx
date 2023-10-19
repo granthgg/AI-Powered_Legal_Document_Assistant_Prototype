2ba45b3c1820ad892be94a8d9acbc1ab4da9530a
Granth Gaurav <69439823+granthgg@users.noreply.github.com>
2023-10-19 21:30:30 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,48 +1,47 @@
 import React from "react";
 import logo from "../assets/logo.png";
 import { AiOutlineCloseCircle as GrClose, AiOutlineHome } from "react-icons/ai";
-import { FiPhoneCall } from "react-icons/fi";
+import { FiPhoneCall, FiInfo } from "react-icons/fi";
 import { MdOutlinePrivacyTip } from "react-icons/md";
 import { FaHandshake } from "react-icons/fa";
-import { FiInfo } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
 const Header = ({ show, handleSideBar }) => {
   return (
-    <div className={`"md:w-full text-white " ${show ? "sidebar active" : "sidebar"}`}>
-      <img src={logo} alt="" className="logo hidden md:block" />
-      <ul className="h-4/5 flex flex-col items-center justify-evenly  text-center">
-        <li>
-          <Link to="/" className="flex flex-row gap-3">
+    <div className={`md:w-full text-white ${show ? "sidebar active" : "sidebar"}`}>
+      <img src={logo} alt="" className="logo hidden md:block mb-4" />
+      <ul className="h-4/5 flex flex-col items-center justify-evenly text-center">
+        <li className="hover:bg-gray-700 p-2 rounded">
+          <Link to="/" className="flex flex-row gap-3 items-center">
             <AiOutlineHome className="block md:hidden" size={"2rem"} />
-            <span className="hidden md:block text-white"> Home</span>
+            <span className="hidden md:block text-white">Home</span>
           </Link>
         </li>
-        <li>
-          <Link to="/about">
+        <li className="hover:bg-gray-700 p-2 rounded">
+          <Link to="/about" className="flex flex-row gap-3 items-center">
             <FiInfo className="block md:hidden" size={"2rem"} />
-            <span className="hidden md:block"> About us</span>
+            <span className="hidden md:block">About us</span>
           </Link>
         </li>
-        <li>
-          <Link to="/contact">
-            <FiPhoneCall className="block md:hidden " size={"2rem"} />
-            <span className="hidden md:block"> Contact Us</span>
+        <li className="hover:bg-gray-700 p-2 rounded">
+          <Link to="/contact" className="flex flex-row gap-3 items-center">
+            <FiPhoneCall className="block md:hidden" size={"2rem"} />
+            <span className="hidden md:block">Contact Us</span>
           </Link>
         </li>
-        <li>
-          <Link to="/privacy">
+        <li className="hover:bg-gray-700 p-2 rounded">
+          <Link to="/privacy" className="flex flex-row gap-3 items-center">
             <MdOutlinePrivacyTip className="block md:hidden" size={"2rem"} />
-            <span className="hidden md:block"> Privacy Policy</span>
+            <span className="hidden md:block">Privacy Policy</span>
           </Link>
         </li>
-        <li>
-          <Link to="/terms">
+        <li className="hover:bg-gray-700 p-2 rounded">
+          <Link to="/terms" className="flex flex-row gap-3 items-center">
             <FaHandshake className="block md:hidden" size={"2rem"} />
-            <span className="hidden md:block"> Terms and Condition</span>
+            <span className="hidden md:block">Terms and Conditions</span>
           </Link>
         </li>
-        <li className=" hover:bg-slate-600">
+        <li className="hover:bg-gray-700 p-2 rounded">
           <GrClose
             onClick={handleSideBar}
             size={"1.5rem"}
