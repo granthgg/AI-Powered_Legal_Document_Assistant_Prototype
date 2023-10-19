d574e71a62664b02d5ac16fc67d616f4e37b8a44
Granth Gaurav <69439823+granthgg@users.noreply.github.com>
2023-10-19 21:14:41 +0530

    Update GenerateToken.jsx

--- client/src/components/GenerateToken.jsx
+++ client/src/components/GenerateToken.jsx
@@ -73,7 +73,7 @@ const GenerateToken = ({ open, handleSideBar }) => {
                 type="submit"
                 className="ml-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-full"
               >
-                Send
+                Draft
               </button>
             </div>
           </form>
