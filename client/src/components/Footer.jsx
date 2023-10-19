53f482017274c4e8b7b086e95d9ff39a9cf89436
Granth Gaurav <69439823+granthgg@users.noreply.github.com>
2023-10-19 19:55:51 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="bg-black text-white py-4">
+    <div className="bg-black text-white py-4 fixed bottom-0 w-full">
       <div className="container mx-auto text-center">
         <p>© 2023 AI-powered Legal Assistant LEGAL DASTAVEZ. All rights reserved.</p>
       </div>
@@ -11,3 +11,5 @@ const Footer = () => {
 };
 
 export default Footer;
+
+
